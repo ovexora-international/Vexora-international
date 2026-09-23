ee24b46bb8acddb7c1c0b666ffebc09deeb6c538
ovexora-international <ovexora@gmail.com>
2026-09-23 16:19:22 +0500

    Update HCAPTCHA_SITE_KEY in config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@
 const CONFIG = {
   API_URL: "PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE",
   GOOGLE_CLIENT_ID: "28032329800-cbomi45s0ioea9mfoqsq6rlk802gqg59.apps.googleusercontent.com",
-  HCAPTCHA_SITE_KEY: "PASTE_YOUR_HCAPTCHA_SITE_KEY_HERE",
+  HCAPTCHA_SITE_KEY: "49de5aea-4cc0-4d06-93e4-aef6ab0c9a7e",
   SITE_NAME: "Vexora International",
   CURRENCY_SYMBOL: "",
   CURRENCY_NAME: "Token",
