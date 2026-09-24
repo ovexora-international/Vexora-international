cdd8a1219ac556404ca13563dfcd8232a88251be
ovexora-international <ovexora@gmail.com>
2026-09-24 13:13:34 +0500

    Update API_URL in config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@
  **************************************************************/
 
 const CONFIG = {
-  API_URL: "PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE",
+  API_URL: "https://script.google.com/macros/s/AKfycbws9tWDKZE94aFmj5si0GvnA0Gp5l6zm5yXR1IEwvnWxwFXZF8ICJ66nRq9LFq3lPDhbw/exec",
   GOOGLE_CLIENT_ID:"28032329800-cbomi45s0ioea9mfoqsq6rlk802gqg59.apps.googleusercontent.com",
   HCAPTCHA_SITE_KEY: "49de5aea-4cc0-4d06-93e4-aef6ab0c9a7e",
   SITE_NAME: "Vexora International",
